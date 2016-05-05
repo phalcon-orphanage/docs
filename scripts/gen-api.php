56a7e69cdcb2f4b1ad79a2455fdf542dba303442
Serghei Iakovlev <sergeyklay@users.noreply.github.com>
2016-05-05 16:04:05 +0300

    Removed hardcoded CPHALCON_DIR (#818)

--- scripts/gen-api.php
+++ scripts/gen-api.php
@@ -12,7 +12,11 @@ if (!extension_loaded('phalcon')) {
     throw new Exception("Phalcon extension is required");
 }
 
-define('CPHALCON_DIR', '/Users/gutierrezandresfelipe/cphalcon/ext/');
+defined('CPHALCON_DIR') || define('CPHALCON_DIR', getenv('CPHALCON_DIR'));
+
+if (!CPHALCON_DIR) {
+    throw new Exception("Need to set CPHALCON_DIR. Fox example: 'export CPHALCON_DIR=/Users/gutierrezandresfelipe/cphalcon/ext/'")
+}
 
 if (!file_exists(CPHALCON_DIR)) {
     throw new Exception("CPHALCON directory does not exist");
